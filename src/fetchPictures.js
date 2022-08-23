5e0897a50a2a24699f80e8eac7482dffbbd67ae5
AndreyKapus <kapustnikov@ukr.net>
2022-08-23 18:43:23 +0300

    need to solve with render

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,10 +1,13 @@
-export function fetchPictures() {
-  fetch(
-    `https://pixabay.com/api/?key=29432159-064ba5645d6ae7f18ff2bb6d2&q&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+const axios = require('axios').default;
+// export { fetchPictures };
+
+export function fetchPictures(query, page, perPage) {
+  axios.defaults.baseURL = 'https://pixabay.com/api/';
+  const KEY = '29432159-064ba5645d6ae7f18ff2bb6d2';
+
+  const response = axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response;
 }
+// console.log(fetchPictures());
--- src/index.js
+++ src/index.js
@@ -4,12 +4,54 @@ const form = document.querySelector('.search-form');
 const searchButton = document.querySelector('button');
 const input = document.querySelector('input');
 
-input.addEventListener('input', onPictureSearch);
+form.addEventListener('submit', onPictureSearch);
+
+let query = '';
 
 function onPictureSearch(e) {
-  e.preventDefault;
-  const searchInput = input.value.trim();
-  console.log(searchInput);
+  e.preventDefault();
+  const query = e.currentTarget.searchQuery.value.trim();
 
-  fetchPictures(searchInput).then(checkData).catch(showError);
+  console.log(query);
+
+  fetchPictures(query).then(checkData).catch(console.log('ошибка'));
+}
+
+function checkData(data) {
+  if (data !== '') {
+    renderPictures(data);
+    console.log('Результат промиса');
+  }
+}
+
+function renderPictures(data) {
+  const markup = data
+    .map(image => {
+      const {
+        id,
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
